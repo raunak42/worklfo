5158d4e6e032f37e58b175c6545fdc16c350a888
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-07-28 11:15:24 +0530

    Button shows text conditionally.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,16 +2,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-
+import { usePathname } from "next/navigation";
 
 export const Navbar: React.FC = () => {
   const [loginCLicked, setLoginClicked] = useState<boolean>(false);
-  
+  const pathname = usePathname();
+
   return (
     <div className="w-full py-[24px] px-[64px] flex flex-row items-center justify-between   ">
       <Image width={180} height={100} alt="logo" src={"/logo.svg"} />
       <Link
-      href={"/Login"}
+        href={pathname==="/Login"?"/":"/Login"}
         className={` bg-[#4534ac] text-[#ffffff] w-[140px] h-[44px] rounded-full font-semibold ${
           !loginCLicked && "hover:-translate-y-1"
         } transition-all duration-200 flex items-center justify-center`}
@@ -24,6 +25,8 @@ export const Navbar: React.FC = () => {
             src={"/spinner-light.svg"}
             className="animate-spin"
           />
+        ) : pathname === "/Login" ? (
+          <h1>Signup</h1>
         ) : (
           <h1>Login</h1>
         )}
