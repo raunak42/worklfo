a59296e489ff685572254c9c105ab6c8e9e3ed99
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-07-29 22:54:16 +0530

    Top loader added, context provider added.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
-import Image from "next/image";
 import { Navbar } from "@/components/Navbar/Navbar";
 import NextTopLoader from "nextjs-toploader";
+import { validateRequest } from "@/auth";
+import { ContextProvier } from "@/providers/ContextProvider";
 
 const manrope = Manrope({ subsets: ["latin"] });
 
@@ -13,18 +14,21 @@ export const metadata: Metadata = {
   icons: ["/brandmark-white.png"],
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { session, user } = await validateRequest();
   return (
     <html lang="en">
-      <body className={manrope.className}>
-        <NextTopLoader color="#4534ac" showSpinner={false} height={4} />
-        <Navbar />
-        <div>{children}</div>
-      </body>
+      <ContextProvier>
+        <body className={manrope.className}>
+          <NextTopLoader color="#4534ac" showSpinner={false} height={4} />
+          {!session && <Navbar />}
+          <div>{children}</div>
+        </body>
+      </ContextProvier>
     </html>
   );
 }
