3b65e084b72e89316a02c4e2ca6e3a47254ff090
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-07-29 22:53:43 +0530

    Server actions to check user in database and to verify password.

---
+++ src/app/actions.ts
@@ -0,0 +1,20 @@
+"use server"
+import { User } from "@/lib/db";
+import bcrypt from "bcryptjs";
+
+
+export const checkUserInDb = async (email: string) => {
+    const existingUser = await User.findOne({
+        email: email
+    });
+
+    return existingUser;    
+}
+
+export const verifyPassword = async (password: string | undefined, hashedPassword: string): Promise<boolean> => {
+    if (password === undefined) {
+        return false;
+    }
+    const verifiedPassword = await bcrypt.compare(password, hashedPassword)
+    return verifiedPassword;
+}
